69f0736cfb37112feddaf72ab10b028fa7741443
shamama-bin-shakil <97785829+Shamama-Bin-Shakil@users.noreply.github.com>
2022-12-03 12:18:18 +0500

    all bug Fixed

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const cors = require("cors");
 // const morgan  = require("morgan");
 // const path  = require("path");
-// const connectDB = require("./server/db/db");
+const connectDB = require("./server/db/db");
 const app = express();
 require("dotenv").config();
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const port = 5000;
 
 // Connection DB
-// connectDB();
+connectDB();
 
 // Use Cors
 app.use(cors());
@@ -21,24 +21,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Use Public Folder
-// app.use(express.static(path.join(__dirname, "./public")));
-
 // Router Auth
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "everything's is fine"
-  })
-});
-
-// app.use("/", require("./server/router/Auth"));
-// // Router Contact
-// app.use("/", require("./server/router/Contact"));
-// // Route Admin
-// app.use("/", require("./server/router/Admin"));
-// // Route Fees
-// app.use("/", require("./server/router/Fees"));
+app.use("/", require("./server/router/Auth"));
+// Router Contact
+app.use("/", require("./server/router/Contact"));
+// Route Admin
+app.use("/", require("./server/router/Admin"));
+// Route Fees
+app.use("/", require("./server/router/Fees"));
 
 
 app.listen(port, () =>
