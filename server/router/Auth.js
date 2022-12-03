cf231e1b9e4801236e9564be547b780a280aaf72
shamama-bin-shakil <97785829+Shamama-Bin-Shakil@users.noreply.github.com>
2022-12-03 13:52:03 +0500

    Update Auth.js
    a

--- server/router/Auth.js
+++ server/router/Auth.js
@@ -10,13 +10,6 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const password = process.env.PASS;
 const accEmail = process.env.EMAIL;
 
-//Route -> (0) REGISTER Account
-route.get("/", (req, res) => {
-  res.json({
-    msg: "welcome to node app"
-  })
-})
-
 //Route -> (1) REGISTER Account
 route.post("/api/register", async (req, res) => {
   try {
